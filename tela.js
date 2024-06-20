2362ce62e41493f741272a2030114e7e0ed5abf1
Jhonny Rafael <88169635+jhonnyrafael01@users.noreply.github.com>
2024-06-20 14:45:29 -0300

    Add files via upload

---
+++ tela.js
@@ -0,0 +1,15 @@
+var moveButton = document.querySelector("button:nth-child(2)");
+console.log('part 1')
+var correctlyButton = document.querySelector("button:first-child");
+
+moveButton.addEventListener("mouseover", function () {
+  var vertical = Math.floor(Math.random() * 400);
+  var horizontal = Math.floor(Math.random() * 400);
+  moveButton.style.position = "absolute";
+  moveButton.style.left = vertical + "px";
+  moveButton.style.top = horizontal + "px";
+});
+
+correctlyButton.addEventListener("click", function () {
+  alert("Resposta correta! Vamo comer um empadão");
+});
