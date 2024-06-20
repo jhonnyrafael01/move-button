4b2a05279dbe90deff852c4eb8a8cbb8d22bda37
Jhonny Rafael <88169635+jhonnyrafael01@users.noreply.github.com>
2024-06-20 14:52:36 -0300

    Update tela.js

--- tela.js
+++ tela.js
@@ -10,6 +10,14 @@ moveButton.addEventListener("mouseover", function () {
   moveButton.style.top = horizontal + "px";
 });
 
+moveButton.addEventListener("click", function () {
+    var vertical = Math.floor(Math.random() * 400);
+    var horizontal = Math.floor(Math.random() * 400);
+    moveButton.style.position = "absolute";
+    moveButton.style.left = vertical + "px";
+    moveButton.style.top = horizontal + "px";
+  });
+
 correctlyButton.addEventListener("click", function () {
   alert("Resposta correta! Vamo comer um empadão");
 });
