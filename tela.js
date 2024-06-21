c73c04c3002aa5782cfc1cc02d2f70ef1c78af0b
Jhonny Rafael <88169635+jhonnyrafael01@users.noreply.github.com>
2024-06-21 10:14:48 -0300

    Update tela.js

--- tela.js
+++ tela.js
@@ -1,21 +1,28 @@
 var moveButton = document.querySelector("button:nth-child(2)");
 var correctlyButton = document.querySelector("button:first-child");
 
-moveButton.addEventListener("mouseover", function () {
-  var vertical = Math.floor(Math.random() * 400);
-  var horizontal = Math.floor(Math.random() * 400);
+function qualquerNome() {
+  var vertical = calcularPosicao("vertical");
+  var horizontal = calcularPosicao("horizontal");
   moveButton.style.position = "absolute";
-  moveButton.style.left = vertical + "px";
-  moveButton.style.top = horizontal + "px";
-});
+  moveButton.style.left = vertical + "vw";
+  moveButton.style.top = horizontal + "vh";
+}
 
-moveButton.addEventListener("click", function () {
-    var vertical = Math.floor(Math.random() * 400);
-    var horizontal = Math.floor(Math.random() * 400);
-    moveButton.style.position = "absolute";
-    moveButton.style.left = vertical + "px";
-    moveButton.style.top = horizontal + "px";
-  });
+function calcularPosicao(tipo) {
+  var min = 4; 
+  var max = 50;
+  var position;
+
+  do {
+    position = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (tipo === "vertical" && position >= 20 && position <= 60);
+
+  return position;
+}
+
+moveButton.addEventListener("mouseover", qualquerNome);
+moveButton.addEventListener("click", qualquerNome);
 
 correctlyButton.addEventListener("click", function () {
   alert("Resposta correta! Vamo comer um empadão");
