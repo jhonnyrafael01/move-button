551e923de4d61ac468e81ba23655df810e65af52
Jhonny Rafael <88169635+jhonnyrafael01@users.noreply.github.com>
2024-06-21 09:10:47 -0300

    Update tela.js

--- tela.js
+++ tela.js
@@ -1,14 +1,5 @@
 var moveButton = document.querySelector("button:nth-child(2)");
-console.log('part 1')
 var correctlyButton = document.querySelector("button:first-child");
-
-moveButton.addEventListener("click", function () {
-    var vertical = Math.floor(Math.random() * 400);
-    var horizontal = Math.floor(Math.random() * 400);
-    moveButton.style.position = "absolute";
-    moveButton.style.left = vertical + "px";
-    moveButton.style.top = horizontal + "px";
-  });
 
 moveButton.addEventListener("mouseover", function () {
   var vertical = Math.floor(Math.random() * 400);
@@ -18,6 +9,14 @@ moveButton.addEventListener("mouseover", function () {
   moveButton.style.top = horizontal + "px";
 });
 
+moveButton.addEventListener("click", function () {
+    var vertical = Math.floor(Math.random() * 400);
+    var horizontal = Math.floor(Math.random() * 400);
+    moveButton.style.position = "absolute";
+    moveButton.style.left = vertical + "px";
+    moveButton.style.top = horizontal + "px";
+  });
+
 correctlyButton.addEventListener("click", function () {
-  alert("Resposta correta! Vamos comer um hamburgão");
+  alert("Resposta correta! Vamo comer um empadão");
 });
